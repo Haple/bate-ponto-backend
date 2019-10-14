17a77979c757be7bea565110851d5f7517a8358f
Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>
2019-10-14 00:01:44 -0300

    Add primeira versão do cadastro de empresas

--- src/empresa/EmpresaController.js
+++ src/empresa/EmpresaController.js
@@ -3,11 +3,33 @@ const db = require('../db');
 module.exports={
 	// Cadastrar empresa
 	async store(req,res){
-		const {cnpj, razaoSocial} = req.body;
-		const result = await db.query(`
-			INSERT 	INTO empresas (cnpj,razao_social)
-			VALUES ($1, $2) RETURNING *`, [cnpj, razaoSocial]);
-		return res.json(result.rows[0]);
+		const {cnpj, razao_social} = req.body;
+		const empresa = await db.query(`
+			INSERT INTO empresas (cnpj,razao_social)
+			VALUES ($1, $2) RETURNING *`,
+			[cnpj, razao_social]);
+		const {cpf,nome,email,senha,celular} = req.body;
+		const usuario = await db.query(`
+			INSERT INTO usuarios
+			(cod_empresa,cpf,nome,email
+				,senha,celular,confirmado)
+			VALUES ($1,$2,$3,$4,$5,$6,false) RETURNING *
+			`,
+			[empresa.rows[0].codigo,cpf,nome,email,
+				senha,celular]);
+		const admin = await db.query(`
+			INSERT INTO administradores
+			(alertar_atraso, cod_usuario)
+			VALUES (false,$1) RETURNING *
+			`,
+			[usuario.rows[0].codigo]);
+		return res.json({
+			empresa: empresa.rows[0],
+			admin: {
+				...usuario.rows[0],
+				...admin.rows[0]
+			}
+		});
 	}
 };
 
