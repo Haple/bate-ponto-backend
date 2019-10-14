7d4cee93c6a984488283cf3a350b0ef82b5da82f
Aleph Santos Oliveira <aleph.oliveira@etec.sp.gov.br>
2019-10-14 00:21:37 -0300

    Refatorado o cadastro de empresas, ainda falta fazer as validações dos dados e verificar se a empresa, o cpf e o email já estão cadastrados

--- src/empresa/EmpresaController.js
+++ src/empresa/EmpresaController.js
@@ -4,35 +4,47 @@ module.exports={
 	// Cadastrar empresa
 	async store(req,res){
 		const {cnpj, razao_social} = req.body;
-		const empresa = await db.query(`
-			INSERT INTO empresas (cnpj,razao_social)
-			VALUES ($1, $2) RETURNING *`,
-			[cnpj, razao_social]);
+		const empresa = await _criarEmpresa(cnpj,razao_social);
 		const {cpf,nome,email,senha,celular} = req.body;
-		const usuario = await db.query(`
-			INSERT INTO usuarios
-			(cod_empresa,cpf,nome,email
-				,senha,celular,confirmado)
-			VALUES ($1,$2,$3,$4,$5,$6,false) RETURNING *
-			`,
-			[empresa.rows[0].codigo,cpf,nome,email,
-				senha,celular]);
-		const admin = await db.query(`
-			INSERT INTO administradores
-			(alertar_atraso, cod_usuario)
-			VALUES (false,$1) RETURNING *
-			`,
-			[usuario.rows[0].codigo]);
+		const usuario = await _criarUsuario(empresa.codigo,cpf
+			,nome,email,senha,celular);
+		const admin = await _criarAdmin(usuario.codigo);
 		return res.json({
-			empresa: empresa.rows[0],
+			empresa: empresa,
 			admin: {
-				...usuario.rows[0],
-				...admin.rows[0]
+				...usuario,
+				...admin
 			}
 		});
 	}
 };
 
-function validarCnpj(){
+async function _criarEmpresa(cnpj,razao_social){
+	const resultado = await db.query(`
+		INSERT INTO empresas (cnpj,razao_social)
+		VALUES ($1, $2) RETURNING *`,
+		[cnpj, razao_social]);
+	return resultado.rows[0];
+}
 
+async function _criarUsuario(cod_empresa,cpf,nome,email,senha,celular){
+	const resultado = await db.query(`
+		INSERT INTO usuarios
+		(cod_empresa,cpf,nome,email
+			,senha,celular,confirmado)
+		VALUES ($1,$2,$3,$4,$5,$6,false) RETURNING *
+		`,
+		[cod_empresa,cpf,nome,email,
+			senha,celular]);
+	return resultado.rows[0];
+}
+
+async function _criarAdmin(cod_usuario){
+	const resultado = await db.query(`
+		INSERT INTO administradores
+		(alertar_atraso, cod_usuario)
+		VALUES (false,$1) RETURNING *
+		`,
+		[cod_usuario]);
+	return resultado.rows[0];
 }
